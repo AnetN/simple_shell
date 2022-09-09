1b5bf74e826d64337a85e3f1d9ae01d168cbbddf
AnetN <101317378+AnetN@users.noreply.github.com>
2022-09-10 00:36:50 +0300

    Update builtins2.c

--- builtins2.c
+++ builtins2.c
@@ -1,122 +1,159 @@
-#include "main.h"
+#innclude "shell.h"
+
 /**
- * add_key - create a new environment variable
- * @vars: pointer to struct of variables
- *
- * Return: void
+ * builtin_exit - exit of the program with the status
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
-void add_key(vars_t *vars)
+int builtin_exit(data_of_program *data)
 {
+	int i;
 
-	unsigned int i;
-
-	char **newenv;
-
-	for (i = 0; vars->env[i] != NULL; i++)
-		;
-
-	newenv = malloc(sizeof(char *) * (i + 2));
-	if (newenv == NULL)
-	{
-		print_error(vars, NULL);
-		vars->status = 127;
-		new_exit(vars);
+	if (data->tokens[1] != NULL)
+	{/*if exists arg for exit, check if is a number*/
+		for (i = 0; data->tokens[1][i]; i++)
+			if ((data->tokens[1][i] < '0' || data->tokens[1][i] > '9')
+				&& data->tokens[1][i] != '+')
+			{/*if is not a number*/
+				errno = 2;
+				return (2);
+			}
+		errno = _atoi(data->tokens[1]);
 	}
-	for (i = 0; vars->env[i] != NULL; i++)
-		newenv[i] = vars->env[i];
-	newenv[i] = add_value(vars->av[1], vars->av[2]);
-	if (newenv[i] == NULL)
-	{
-
-		print_error(vars, NULL);
-		free(vars->buffer);
-		free(vars->commands);
-		free(vars->av);
-		free_env(vars->env);
-		free(newenv);
-		exit(127);
-	}
-	newenv[i + 1] = NULL;
-	free(vars->env);
-	vars->env = newenv;
-}
-/**
- * find_key - finds an environment variable
- * @env: array of environment variables
- * @key: environment variable to find
- *
- * Return: pointer to address of the environment variable
- */
-char **find_key(char **env, char *key)
-{
-	unsigned int i, j, len;
-
-	len = _strlen(key);
-	for (i = 0; env[i] != NULL; i++)
-	{
-		for (j = 0; j < len; j++)
-			if (key[j] != env[i][j])
-				break;
-		if (j == len && env[i][j] == '=')
-			return (&env[i]);
-	}
-	return (NULL);
-}
-/**
- * add_value - create a new environment variable string
- * @key: variable name
- * @value: variable value
- *
- * Return: pointer to the new string;
- */
-char *add_value(char *key, char *value)
-{
-	unsigned int len1, len2, i, j;
-
-	char *new;
-
-	len1 = _strlen(key);
-	len2 = _strlen(value);
-	new = malloc(sizeof(char) * (len1 + len2 + 2));
-	if (new == NULL)
-		return (NULL);
-	for (i = 0; key[i] != '\0'; i++)
-		new[i] = key[i];
-	new[i] = '=';
-	for (j = 0; value[j] != '\0'; j++)
-		new[i + 1 + j] = value[j];
-	new[i + 1 + j] = '\0';
-	return (new);
-}
-/**
- * _atoi - converts a string into an integer
- * @str: string to convert
- *
- * Return: the integer value, or -1 if an error occurs
- */
-int _atoi(char *str)
-{
-	unsigned int i, digits;
-
-	int num = 0, num_test;
-
-	num_test = INT_MAX;
-	for (digits = 0; num_test != 0; digits++)
-		num_test /= 10;
-
-	for (i = 0; str[i] != '\0' && i < digits; i++)
-	{
-		num *= 10;
-		if (str[i] < '0' || str[i] > '9')
-			return (-1);
-		if ((i == digits - 1) && (str[i] - '0' > INT_MAX % 10))
-			return (-1);
-		num += str[i] - '0';
-		if ((i == digits - 2) && (str[i + 1] != '\0') && (num > INT_MAX / 10))
-			return (-1);
-	}
-	if (i > digits)
-		return (-1);
-	return (num);
+	free_all_data(data);
+	exit(errno);
 }
 
+/**
+ * builtin_cd - change the current directory
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int builtin_cd(data_of_program *data)
+{
+	char *dir_home = env_get_key("HOME", data), *dir_old = NULL;
+	char old_dir[128] = {0};
+	int error_code = 0;
+
+	if (data->tokens[1])
+	{
+		if (str_compare(data->tokens[1], "-", 0))
+		{
+			dir_old = env_get_key("OLDPWD", data);
+			if (dir_old)
+				error_code = set_work_directory(data, dir_old);
+			_print(env_get_key("PWD", data));
+			_print("\n");
+
+			return (error_code);
+		}
+		else
+		{
+			return (set_work_directory(data, data->tokens[1]));
+		}
+	}
+	else
+	{
+		if (!dir_home)
+			dir_home = getcwd(old_dir, 128);
+
+		return (set_work_directory(data, dir_home));
+	}
+	return (0);
+}
+
+/**
+ * set_work_directory - set the work directory
+ * @data: struct for the program's data
+ * @new_dir: path to be set as work directory
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int set_work_directory(data_of_program *data, char *new_dir)
+{
+	char old_dir[128] = {0};
+	int err_code = 0;
+
+	getcwd(old_dir, 128);
+
+	if (!str_compare(old_dir, new_dir, 0))
+	{
+		err_code = chdir(new_dir);
+		if (err_code == -1)
+		{
+			errno = 2;
+			return (3);
+		}
+		env_set_key("PWD", new_dir, data);
+	}
+	env_set_key("OLDPWD", old_dir, data);
+	return (0);
+}
+
+/**
+ * builtin_help - shows the environment where the shell runs
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int builtin_help(data_of_program *data)
+{
+	int i, length = 0;
+	char *mensajes[6] = {NULL};
+
+	mensajes[0] = HELP_MSG;
+
+	/* validate args */
+	if (data->tokens[1] == NULL)
+	{
+		_print(mensajes[0] + 6);
+		return (1);
+	}
+	if (data->tokens[2] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+	mensajes[1] = HELP_EXIT_MSG;
+	mensajes[2] = HELP_ENV_MSG;
+	mensajes[3] = HELP_SETENV_MSG;
+	mensajes[4] = HELP_UNSETENV_MSG;
+	mensajes[5] = HELP_CD_MSG;
+
+	for (i = 0; mensajes[i]; i++)
+	{
+		length = str_length(data->tokens[1]);
+		if (str_compare(data->tokens[1], mensajes[i], length))
+		{
+			_print(mensajes[i] + length + 1);
+			return (1);
+		}
+	}
+	/*if there is no match, print error and return -1 */
+	errno = EINVAL;
+	perror(data->command_name);
+	return (0);
+}
+
+/**
+ * builtin_alias - add, remove or show aliases
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int builtin_alias(data_of_program *data)
+{
+	int i = 0;
+
+	/* if there are no arguments, print all environment */
+	if (data->tokens[1] == NULL)
+		return (print_alias(data, NULL));
+
+	while (data->tokens[++i])
+	{/* if there are arguments, set or print each env variable*/
+		if (count_characters(data->tokens[i], "="))
+			set_alias(data->tokens[i], data);
+		else
+			print_alias(data, data->tokens[i]);
+	}
+
+	return (0);
+}
